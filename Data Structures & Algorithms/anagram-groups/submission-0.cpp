faf5a9cb49873aefe581f1141d6ea5f35973615e
Himanshu Singh Dhapola <66303885+HimanshuSinghDhapola@users.noreply.github.com>
2026-04-17 16:39:25 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> mpp;
+        for(int i=0; i<strs.size(); i++){
+            string sortedStr = strs[i];
+            sort(sortedStr.begin(), sortedStr.end());
+            mpp[sortedStr].push_back(strs[i]);
+        }
+        for(auto &it: mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
