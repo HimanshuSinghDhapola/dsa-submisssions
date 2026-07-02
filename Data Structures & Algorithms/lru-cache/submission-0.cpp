1f6f8ac8d9119f36bb6bbd8d03397d22f7a81a12
Himanshu Singh Dhapola <66303885+HimanshuSinghDhapola@users.noreply.github.com>
2026-07-02 08:21:20 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.cpp
@@ -0,0 +1,34 @@
+class LRUCache {
+public:
+    vector<pair<int, int>> cache;
+    int n;
+    LRUCache(int capacity) {
+        n=capacity;
+    }
+    
+    int get(int key) {
+        for(int i=0; i<cache.size(); i++){
+            if(cache[i].first == key){
+                pair<int, int> temp = cache[i];
+                cache.erase(cache.begin()+i);
+                cache.push_back(temp);
+                return temp.second;
+            }
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        for(int i=0; i<cache.size(); i++){
+            if(cache[i].first == key){
+                cache.erase(cache.begin()+i);
+                cache.push_back({key, value});
+                return;
+            }
+        }
+        if(n == cache.size()){
+            cache.erase(cache.begin());
+        }
+        cache.push_back({key, value});
+    }
+};
